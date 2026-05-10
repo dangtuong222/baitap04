e7bc93b209a68aed803b4d69cf490b1bf01675c6
NDTuong <nguyendangtuong2005@gmail.com>
2026-05-10 22:27:06 +0700

    register01

--- src/config/configdb.js
+++ src/config/configdb.js
@@ -2,15 +2,30 @@ import { Sequelize } from 'sequelize';
 //const { Sequelize } = require('sequelize');//ES5 module
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('node_fulltask', 'root', '171005', {
+const sequelize = new Sequelize('node_fulltask', 'root', '123456', {
     host: 'localhost',
     dialect: 'mysql',
     logging: false
 });
 
+const ensureUsersRoleColumn = async () => {
+    const queryInterface = sequelize.getQueryInterface();
+    const tableName = 'Users';
+
+    const columns = await queryInterface.describeTable(tableName);
+    if (!Object.prototype.hasOwnProperty.call(columns, 'role')) {
+        await queryInterface.addColumn(tableName, 'role', {
+            type: Sequelize.STRING,
+            allowNull: true,
+            defaultValue: 'user'
+        });
+    }
+};
+
 let connectDB = async () => {
     try {
         await sequelize.authenticate();
+        await ensureUsersRoleColumn();
         console.log('Connection has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
